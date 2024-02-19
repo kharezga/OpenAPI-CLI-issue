44fff3fb60bf15b56dca3a282b2b06eed0604238
Kacper Harezga <kharezga@bigtime.net>
2024-02-19 13:44:06 +0100

    Add mock project

---
+++ MyApp/MyCoolPackage/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyCoolPackage",
+    platforms: [
+        .iOS(.v16),
+    ],
+    products: [
+        .library(
+            name: "MyCoolPackage",
+            targets: ["MyCoolPackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.2.1")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/RocketLaunchpad/Keychain", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "MyCoolPackage",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "Keychain", package: "Keychain")
+            ],
+            path: "./Sources",
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]),
+        .testTarget(
+            name: "MyCoolPackageTests",
+            dependencies: ["MyCoolPackage"]),
+    ]
+)
